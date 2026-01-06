8e8193b780f0f8abbc9a169b6a6df2ba6af324d8
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-01-06 19:37:29 +0530

    Class Generics

---
+++ Day 8/dist/class.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class KeyValuePair {
+    _key;
+    _value;
+    constructor(_key, _value) {
+        this._key = _key;
+        this._value = _value;
+    }
+    get key() {
+        return this._key;
+    }
+    get value() {
+        return this._value;
+    }
+    display() {
+        console.log(`Key: ${this._key}, Value: ${this._value}`);
+    }
+}
+const student1 = new KeyValuePair("age", 30);
+student1.display();
+const product1 = new KeyValuePair("101", "Laptop");
+product1.display();
+const userData = new KeyValuePair("user1", { name: "Alice", email: "alice@example.com" });
+userData.display();
+class Logger {
+    log(value) {
+        console.log(`Log entry: ${value}`);
+    }
+}
+const logger = new Logger();
+logger.log("This is a log message.");
+//# sourceMappingURL=class.js.map
--- Day 8/src/class.ts
+++ Day 8/src/class.ts
@@ -15,5 +15,24 @@ class KeyValuePair<K, V> {
     }
 }
 
+interface User{
+    name : string;
+    email : string;
+}
+
 const student1 = new KeyValuePair<string, number>("age", 30);
 student1.display();
+const product1 = new KeyValuePair<string, string>("101", "Laptop");
+product1.display();
+
+const userData = new KeyValuePair<string, User>("user1", {name: "Alice", email: "alice@example.com"});
+userData.display();
+
+
+class Logger{
+    log<T>(value: T): void {
+        console.log(`Log entry: ${value}`);
+    }
+}
+const logger = new Logger();
+logger.log<string>("This is a log message.");
